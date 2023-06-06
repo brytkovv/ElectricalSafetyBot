33627d0fcc6bc6260e8ce4857c213bef34e08b69
Брытков Вячеслав Вячеславович <vbrytkov@rtrn.ru>
2023-06-06 14:24:28 +0300

    cancel filters

--- src/bot/handlers/cancel.py
+++ src/bot/handlers/cancel.py
@@ -1,7 +1,7 @@
 """ This file represents a cancel logic """
 
 from aiogram import Router, types, F
-from aiogram.filters import Command, Text
+from aiogram.filters import Command, Text, or_f, and_f
 from aiogram.fsm.context import FSMContext
 
 from src.bot.lexicon import settings_text
@@ -11,8 +11,10 @@ cancel_router = Router(name="cancel")
 
 
 @cancel_router.message(
-        *[state for state in [SettingsStates.set_number_of_questions, SettingsStates.set_theme, SettingsStates.set_correct_answer_alert] if state],
-        *[i for i in [F.text == settings_text.BUTTON_CANCEL, Command(commands=["cancel"]), Text(text="отмена", ignore_case=True)] if i]
+        and_f(
+            or_f(SettingsStates.set_number_of_questions, SettingsStates.set_theme, SettingsStates.set_correct_answer_alert),
+            or_f(F.text == settings_text.BUTTON_CANCEL, Command(commands=["cancel"]), Text(text="отмена", ignore_case=True))
+        )
 )
 async def cancel(message: types.Message, state: FSMContext):
     await message.answer(
